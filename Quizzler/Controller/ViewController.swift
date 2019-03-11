1a8c010704ba9b67711ebf7f31721479394aa737
murad <muradkv@gmail.com>
2019-03-11 22:21:23 +0300

    Add questionLabel

--- Quizzler/Controller/ViewController.swift
+++ Quizzler/Controller/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
